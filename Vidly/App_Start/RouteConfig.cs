cefa0a20b7ece3caf44ddf91f8960f2d7b1e2b27
sooraj.v <soorajv.c@ivedix.com>
2016-07-14 23:07:47 +0530

    First Mosh Project commit

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "Movies/released/{year}/{month}",
+            //    new { Controller = "Movie", action = "ByRelaseDate", year = UrlParameter.Optional, month = UrlParameter.Optional });
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Namee = "North 24 Kadam" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("Id=" + movieId);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        //[Route("Movies/released/{year?}/{month?:regex(\\d{2}):range(1,12)}")]
+        [Route("Movies/released/{year?}/{month?}")]
+        public ActionResult ByRelaseDate(int? year, int? month)
+        {
+            if (!year.HasValue)
+                year = DateTime.Now.Year;
+            if (!month.HasValue)
+                month = DateTime.Now.Month;
+            return Content(year + "/" + month);
+        }
+    }
+}
